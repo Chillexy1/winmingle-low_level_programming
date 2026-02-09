9c7a1fb02b0aeb81dbc5d0493219f2aaedfe6296
chillexy <chillexystev96@gmail.com>
2026-02-09 20:21:52 +0100

    directory containing winmingle-low_level_programming assignments

---
+++ 0x00_hello-world/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+* Author: Chillexy Steven
+* Program: WinMingle Community C Training
+* Description: this main.c file receives an output from our preprocessor
+*/
+
+int main(void){
+    printf("hello world");
+    return(0);
+}
