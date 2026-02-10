9e9543f7aa6a3ea829514cc08694321143049e5d
chillexy <chillexystev96@gmail.com>
2026-02-10 16:58:34 +0100

    this is c code that prints out a line of statement using the puts function

---
+++ 0x00_hello-world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*
+ * this is a test to print out a line of statement using the function puts 
+ */
+int main(void){
+    puts("\"Programming is like building a multilingual puzzle\n");
+    return(0);
+}
