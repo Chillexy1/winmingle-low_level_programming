25004a9224672a37318ebe53068f058a09596dda
chillexy <chillexystev96@gmail.com>
2026-02-15 20:14:32 +0100

    printing alphabets in lower_case using only two putchar calls

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* 
+ *Authour: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: printing alphabetas in lowercase and uppercase
+ * */
+
+int main(void){
+    char c;
+    for(c = 'a'; c <= 'z'; c++){
+        putchar(c);
+    
+    }
+    putchar('\n');
+    return (0);
+}
