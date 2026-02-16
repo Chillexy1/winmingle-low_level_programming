238f18e002ddd4cb7dab9f3881faca4224ae5814
chillexy <chillexystev96@gmail.com>
2026-02-16 18:42:56 +0100

    printing all digits of base 10 starting from 0 using the putchar fuction without the use of char variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+*Authour: Chillexy Steven                                                          *Program: WinMingle Community C Training
+*Description: printing all digit number of base 10 starting from 0
+*/
+
+int main(void){
+    int base = 10;
+    int n;
+    for(n = 0; n < base ; n++ ){
+        putchar(n + '0');
+    }
+    putchar('\n');
+    return(0);
+}
