f4188ef9171eac6767ac88633757101149cc7018
chillexy <chillexystev96@gmail.com>
2026-02-22 22:34:51 +0100

    this function computes the absolute value of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/*
+*Authour: Chillexy Steven       
+*Program: WinMingle Community C Training
+*Description: this function computes the absolute value of an integer
+*/
+
+int _abs(int j)
+{
+    if(j < 0)
+        return -j;
+    else
+        return j;
+
+}
