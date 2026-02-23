5511ae83b71e516022d2eacda9c1f0fb68f891fd
chillexy <chillexystev96@gmail.com>
2026-02-23 23:26:53 +0100

    function for adding two int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/*
+*Authour: Chillexy Steven     
+*Program: WinMingle Community C Training
+*Description: this is a function that adds two int together and return it's outcome
+*/
+
+int add(int a, int b)
+{
+return a + b;
+}
