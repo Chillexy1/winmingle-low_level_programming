df17bdda0056629afdaae01ea4c6318ede004aac
chillexy <chillexystev96@gmail.com>
2026-02-26 22:32:16 +0100

    this is the runner file of the function 101-natural.c

---
+++ 0x02-functions_nested_loops/101-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void){
+    int result = print_natural(3, 5);
+    printf("%d\n", result);
+    return (0);
+}
