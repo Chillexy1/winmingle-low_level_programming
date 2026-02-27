3eddc43e2c1998e4b15e2050b597f70c04c4a76c
chillexy <chillexystev96@gmail.com>
2026-02-27 19:05:51 +0100

    this is a fuction that prints out the first 50 fibonacci number starting with 1 & 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/*
+*Authour: Chillexy Steven
+*Program: WinMingle Community C Training
+*Description: this finction prints out the first 50 fibonacci number starting with 1 and 2.
+*/
+
+void print_fibonacci(void){
+    long long current = 2;
+    long long previous = 1;
+    long long next;
+    int n =1;
+
+    while(n <= 50){
+        if(n != 1){
+            printf(", ");
+        }
+        printf("%lld", current);
+
+        next = current + previous;
+        previous = current;
+        current = next;
+
+        n++;
+    }
+}
