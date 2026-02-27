96b558572a8a823b5118250b5554eeb2ea261b1b
chillexy <chillexystev96@gmail.com>
2026-02-27 23:30:33 +0100

    this is a runner file for the function 103-fibonaci.c

---
+++ 0x02-functions_nested_loops/103-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void){
+    int sum_of_even =  return_fibonacci();
+    printf("the sum of the even_value_terms is: %d",sum_of_even);
+    printf("\n");
+
+    return 0;
+}
