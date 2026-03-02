67be140ccdefe5618ac85b04431e572986e7e496
chillexy <chillexystev96@gmail.com>
2026-03-02 18:35:58 +0100

    this are the declaration functions that contains both the converting fumction amd the print_remaining_days function

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void){   
+    int month = 2,day = 29,year = 2000;
+    
+    printf("Date: %02d/%02d/%04d\n",month,day,year);
+
+    int days_of_month = convert_days(month, day, year);
+
+    print_remaining_days(month, days_of_month, year );
+
+
+
+    return(0);
+}
+
+
