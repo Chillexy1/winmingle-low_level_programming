d28333044f289e55f10cc9e9462ab90a9ab932b6
chillexy <chillexystev96@gmail.com>
2026-03-04 00:17:24 +0100

    this is a function that updates an int in another function to value 98 using a pointer

---
+++ pointers_arrays_atrings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+*Authour: Chillexy Steven         
+*Program: WinMingle Community C Training
+*Description:this function takes a pointer as an int to a parameter and updates the value it points at to 98. 
+*compillation: -Wall -Werror -Wextra -pedantic -std=gnu89
+*/
+
+void reset_to_98(int *n) {
+    *n = 98;
+
+}
