03dcd5827f32a304932acded8c8922848fed3fe5
chillexy <chillexystev96@gmail.com>
2026-03-07 01:41:10 +0100

    main function that calls our prototype declared

---
+++ pointers_arrays_strings/8-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * main function to call our declared duncts.
+ * 
+ * must always return 0;
+ * */
+
+int main(void){
+    int n = 5;
+    int a[] = {98, 402, -198, 298, -1024};
+    print_array(a, n);
+
+    return(0);
+}
