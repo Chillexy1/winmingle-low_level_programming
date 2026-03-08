6ad0eeef73aed8d121cc073b3a7f81e24b08f96c
chillexy <chillexystev96@gmail.com>
2026-03-08 12:49:11 +0100

    i needed the length of my src to declare to my dest file so this function fetches that purpose

---
+++ pointers_arrays_strings/9-len.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * rhis returns the len of src so we can use it in the code
+ * */
+
+int _len(char *src){
+    int n = 0;
+
+    n = strlen(src);
+    return n;
+}
