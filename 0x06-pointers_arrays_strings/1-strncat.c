26fdedd8a40fdf945ebe50c5ad6cffc5aa534ccb
chillexy <chillexystev96@gmail.com>
2026-03-11 00:32:24 +0100

    this is a function that concatenates two strings using n bytes from src

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/*
+*Authour: Chillexy Steven 
+*Program: WinMingle Community C Training
+*Description: function to concatebates two strings using n bytes from src
+*/
+
+char *_strncat(char *dest, char *src, int n){
+    return strncat(dest,src,n);
+
+
+}
