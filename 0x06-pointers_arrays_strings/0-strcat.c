f27df8af067c1986014da9a99e21851c431e3edb
chillexy <chillexystev96@gmail.com>
2026-03-10 22:27:21 +0100

    function to concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/*
+*Authour: Chillexy Steven
+*Program: WinMingle Community C Training
+*Description: this is a function that concatenates two strings
+*compilation: gcc -Wall -Werror -Wextra -pedantic -std=gnu89
+*/
+
+char *_strcat(char *dest, char *src){
+    return strcat(dest, src);
+
+}
