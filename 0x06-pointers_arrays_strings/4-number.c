f9da69ddee69b0f5b99b186b445adf443cf2aeef
chillexy <chillexystev96@gmail.com>
2026-03-12 00:24:16 +0100

    we use this function to print out numbers cause we're told to only print with _putchar

---
+++ 0x06-pointers_arrays_strings/4-number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ * function to print numbers using _putchar
+ * */
+
+void print_number(int n){
+    if(n > 10){
+        print_number(n/10);
+    }
+    _putchar((n%10) + '0');
+}
