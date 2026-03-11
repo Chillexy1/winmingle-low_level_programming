d1b45498a0cd006b253b7bf0f680dce03df1f0e7
chillexy <chillexystev96@gmail.com>
2026-03-11 01:08:29 +0100

    this is a function that copies two strings using n bytes from src

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/*
+*Authour: Chillexy Steven                                                           
+*Program: WinMingle Community C Training
+*Description: this function copies a string
+*/
+
+char *_strncpy(char *dest, char *src, int n){
+    return strncpy(dest, src, n);
+}
