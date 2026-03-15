f3f3ec6e2d0e03624d69c51d779aebfb86ef8bc5
chillexy <chillexystev96@gmail.com>
2026-03-15 14:31:44 +0100

    this function locates a substring from source(needle) to destination(haystack)

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/*
+*Authour: Chillexy Steven   
+*Program: WinMingle Community C Training
+*Description: this function locates a substring from source(src) to destination(dst)
+*/
+
+char *_strstr(char *haystack, char *needle){
+    return strstr(haystack, needle);
+}
