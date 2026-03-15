2fb5d8c949a9760d4c21410c00d10bac14756eae
chillexy <chillexystev96@gmail.com>
2026-03-15 14:10:41 +0100

    returns the first occurence of strings in destination(dst) available in source(src)"

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/*
+*Authour: Chillexy Steven     
+*Program: WinMingle Community C Training
+*Description: finds first occurrence in s of any bytes in accept.  
+*/
+
+char *_strpbrk(char *s, char *accept){
+    return strpbrk(s, accept);
+}
