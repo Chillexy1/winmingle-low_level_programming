4d2322b679e58439875925a636e35cef873bd117
chillexy <chillexystev96@gmail.com>
2026-03-15 13:09:04 +0100

    learning strchr that returns the first occurrence of a character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/*
+*Authour: Chillexy Steven 
+*Program: WinMingle Community C Training
+*Description: this is a function that locates a character to a pointer and return it's first occurrencen
+*/
+
+char *_strchr(char *s, char c){
+    return strchr(s, c);
+
+}
