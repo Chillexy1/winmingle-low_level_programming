97bf9e01ec56c281ea02579c87e567d77575c0e2
chillexy <chillexystev96@gmail.com>
2026-03-15 13:48:08 +0100

    returns lenght of a prefix substring matching a pointer argument(accept)

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/*
+*Authour: Chillexy Steven   
+*Program: WinMingle Community C Training
+*Description: this is a function that returns lenght of a prefix substring matching bytes from the declared pointer (accept). 
+*/
+
+unsigned int _strspn(char *s, char *accept){
+    return strspn(s, accept);
+}
