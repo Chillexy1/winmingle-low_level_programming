d061780f46e90d0636ace9f0b6f7dd3b0eda6b51
chillexy <chillexystev96@gmail.com>
2026-03-16 02:37:15 +0100

    directing the value of a src to a dst with a pointer(double pointer)

---
+++ 0x07-pointers_arrays_strings/8-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/*
+*Authour: Chillexy Steven   ;                                                            *Program: WinMingle Community C Training
+*Description: we're setting s value of a pointer to a char.
+*/
+
+void set_string(char **s, char *to){
+    *s = to;
+
+}
