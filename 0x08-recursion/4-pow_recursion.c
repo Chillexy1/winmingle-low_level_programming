e64c9479fe263fac1d6b27a88a14fa8f0da82d6f
chillexy <chillexystev96@gmail.com>
2026-03-17 21:38:13 +0100

    using recursion to perform an exponential of number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <math.h>
+
+/*
+*Authour: Chillexy Steven
+*Program: WinMingle Community C Training
+*Description: returns the value of x raised to the power of y
+*/
+
+int _pow_recursion(int x, int y){
+   if(y < 0)
+       return -1;
+
+   if(y == 0)
+       return 1;
+
+   return x * _pow_recursion(x, y - 1);
+
+
+}
