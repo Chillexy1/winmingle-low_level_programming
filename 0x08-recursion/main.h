5d20a4a6be2a66f81dc8a17caccd27a4dc885c26
chillexy <chillexystev96@gmail.com>
2026-03-20 16:29:35 +0100

    header files containing all prototype in this lesson

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,24 @@
+void _puts_recursion(char *s);
+
+int _putchar(char c);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+void print_number(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+int _sqrt_root(int n, int root);
+
+int is_prime_number(int n);
+int prime_check(int n, int root);
+
+int is_palindrome(char *s);
+int checks_palindrome(char *s, int start, int end);
+
+int wildcmp(char *s1, char *s2);
