bbf899abc6ea4fe1f154381ff77283db0a22853c
chillexy <chillexystev96@gmail.com>
2026-03-21 22:01:45 +0100

    this program prints the number of arguments passed to it, excluding the program name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+*Authour: Chillexy Steven
+*Program: WinMingle Community C Training
+*Description: this program prints out the number of arguments passed into it
+*/
+
+int main(int argc, char *argv[]){
+    printf("%d\n", argc -1);
+
+
+    return 0;
+}
