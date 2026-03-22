bdaa0862b5b728f2883b021e2ec2b4245ee5affd
chillexy <chillexystev96@gmail.com>
2026-03-22 14:12:52 +0100

    this program accepts and multiplies only two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+*Authour: Chillexy Steven      
+*Program: WinMingle Community C Training
+*Description: this program multiplies two numbers
+*/
+
+int main(int argc, char *argv[]){
+
+    int i, mul;
+
+    mul = 1;
+
+    if(argc < 3){
+        printf("Error\n");
+        return 1;
+    }
+
+    else if(argc >3){
+        printf("input is above expected requirement, E.G: 10, 20");
+        return 1;
+    }
+
+
+    for(i = 1; i < argc; i++){
+
+        mul *= atoi(argv[i]);
+    }
+    printf("%d\n", mul);
+
+
+    return 0;
+}
