d1a6bdd450b2fa11e8337914aae132c9603c5629
chillexy <chillexystev96@gmail.com>
2026-03-28 18:10:31 +0100

    concatinating a new strings into a buffer

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/*
+*Authour: Chillexy Steven  
+*Program: WinMingle Community C Training
+*Description: we're to conncatenate two strings into a new buffer
+*/
+
+char *str_concat(char *s1, char *s2){
+    int i,j;
+    int len1 = 0, len2 = 0;
+
+    char *str;
+
+    if(s1 == NULL)
+        s1 = "";
+
+    if(s2 == NULL)
+        s2 = "";
+
+    while(s1[len1] != '\0')
+        len1++;
+
+    while(s2[len2] != '\0')
+        len2++;
+    str = malloc(sizeof(char) + (len1 + len2 +1));
+    if(str == NULL)
+        return NULL;
+
+    for(i = 0; i < len1; i++)
+        str[i] = s1[i];
+
+    for(j = 0; j < len2; j++)
+        str[i + j]= s2[j];
+
+    str[i + j] ='\0';
+
+    return str;
+
+}
