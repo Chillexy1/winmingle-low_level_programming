aeb12cc73740797c8eec6c2d0f89a74501e047ac
chillexy <chillexystev96@gmail.com>
2026-04-04 19:08:29 +0100

    this is a header-file that defines a macro name with a size value

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+
+/*
+*Author: Chillexy Steven    
+*Program: WinMingle Community C Training
+*Description: we're creating a program file that defines a macro named 'SIZE' with a value 1024
+*/
+
+#define SIZE 1024
