033ee1d0b6105d17adc39b0384f3db9dbcd99db1
chillexy <chillexystev96@gmail.com>
2026-04-04 22:11:57 +0100

    this is a function that computes the value of x int

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+
+/*
+*Author: Chillexy Steven      
+*Program: WinMingle Community C Training
+*Description: this is function program that computes the vakue of x
+*/
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
