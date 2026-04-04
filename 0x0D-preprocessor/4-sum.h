3e362a83d6e8e33a02e7cf2e7939af2642bc8457
chillexy <chillexystev96@gmail.com>
2026-04-04 22:42:53 +0100

    this is a macro program that computes two integer values

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+
+/*
+*Author: Chillexy Steven   
+*Program: WinMingle Community C Training
+*Description: this function sums the the input of two macro integers
+*/
+
+#define SUM(x, y) (((x) < 0 ? -(x): (x)) + ((y) <  0 ? -(y): (y)))
