178c2892153479cb4ff7cf338f8ce8f08761d9c0
chillexy <chillexystev96@gmail.com>
2026-05-28 18:20:02 +0100

    adds a two number multiplication function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/*
+ *Author: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: this is function that multiplies two intger number
+ */
+
+int mul(int a, int b) { return a * b; }
