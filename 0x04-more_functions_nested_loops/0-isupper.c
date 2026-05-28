7a6dd1650431cab60136d2ba344612abaaf595f9
chillexy <chillexystev96@gmail.com>
2026-05-28 14:10:40 +0100

    adds a function that checks if a character is an uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ *Author: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: this function checks if a given character is an uppercase or not
+ */
+
+int _isupper(int c) {
+  if (c >= 'A' && c <= 'Z')
+    return 1;
+  else
+
+    return 0;
+}
