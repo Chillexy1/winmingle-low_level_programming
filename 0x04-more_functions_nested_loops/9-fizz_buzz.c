819701b3e52f0788b761518ea093bda81487fb70
chillexy <chillexystev96@gmail.com>
2026-05-29 01:37:12 +0100

    adds a program that prints the number 1 - 100 of fizz_buzz game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ *Author: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: FIZZ BUZZ -this is the fizz_buzz program
+ */
+
+int main(void) {
+  int number;
+  for (number = 1; number <= 100; number++) {
+    if ((number % 3 == 0) && (number % 5 == 0))
+      printf("FizzBuzz ");
+    else if (number % 3 == 0)
+      printf("Fizz ");
+    else if (number % 5 == 0)
+      printf("Buzz ");
+    else
+      printf("%d ", number);
+  }
+  printf("\n");
+
+  return (0);
+}
