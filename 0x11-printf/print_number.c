3ba635c3d48ae6b0e623f29f0f9105aafd26d4de
chillexy <chillexystev96@gmail.com>
2026-07-28 18:17:34 +0100

    adds a function that prints any number of an int data type

---
+++ 0x11-printf/print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ *Author: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: fuction that prints any interger number
+ */
+
+void print_number(int n, char *buffer,
+                  int *index) { /* checks if number is less than 0 */
+  if (n < 0) {
+    store_buffer('-', buffer, index);
+    n = -n;
+  }
+
+  if (n >= 10)
+    print_number(n / 10, buffer, index);
+
+  store_buffer((n % 10) + '0', buffer, index);
+}
