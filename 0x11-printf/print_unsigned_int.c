01b84a25e4cd8a39652f71883c541499f64130a8
chillexy <chillexystev96@gmail.com>
2026-07-28 18:24:54 +0100

    adds a function implimentation of an unsigned int values

---
+++ 0x11-printf/print_unsigned_int.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+ *Author: Chillexy Steven
+ *Program: WinMingle Community C Training
+ *Description: prints an unsigned int value
+ */
+
+void print_unsigned_int(unsigned int n, char *buffer, int *index) {
+  if (n >= 10)
+    print_unsigned_int(n / 10, buffer, index);
+
+  store_buffer('0' + (n % 10), buffer, index);
+}
