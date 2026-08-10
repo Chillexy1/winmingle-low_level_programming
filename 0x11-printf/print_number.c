c31cf23bca70ea7ea796a7d1ef195e4034ca40f1
chillexy <chillexystev96@gmail.com>
2026-08-10 21:51:13 +0100

    adds a function that print integer values

--- 0x11-printf/print_number.c
+++ 0x11-printf/print_number.c
@@ -8,13 +8,16 @@
 
 void print_number(int n, char *buffer,
                   int *index) { /* checks if number is less than 0 */
+  long int num; // use this to deal with any overflow of min or max value
+  num = n;
+
   if (n < 0) {
     store_buffer('-', buffer, index);
-    n = -n;
+    num = -num;
   }
 
-  if (n >= 10)
-    print_number(n / 10, buffer, index);
+  if (num >= 10)
+    print_number(num / 10, buffer, index);
 
-  store_buffer((n % 10) + '0', buffer, index);
+  store_buffer((num % 10) + '0', buffer, index);
 }
