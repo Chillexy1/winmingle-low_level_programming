f2fef754827f5c873ca36c98045e48e4d6922275
chillexy <chillexystev96@gmail.com>
2026-08-20 22:53:50 +0100

    feat: this a function that adds a new node to the beginning of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/*
+*Author: Chillexy Steven
+
+ *Program: WinMingle Community C Training
+
+*Description: this a function that adds a new node at the very beggining of a
+list_t list
+*
+* we're allowed to use the "strdup"
+*/
+
+list_t *add_node(list_t **head, const char *str) {
+  list_t *new;
+
+  new = malloc(sizeof(*new)); // allocates size for the new node
+  if (new == NULL)
+    return (NULL);
+
+  new->str = strdup(str); // duplicates str
+
+  if (new->str == NULL) // return NULL if it fails
+  {
+    free(new);
+    return (NULL);
+  }
+
+  new->len = strlen(str); // adds the length of the str to the structure memeber
+
+  new->next = *head;
+  *head = new;
+
+  return (new); // returns the address of the new element
+}
