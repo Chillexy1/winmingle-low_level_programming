204a8354e65060d5547c365c30280fdfc181cbb0
Chillexy <chillexystev96@gmail.com>
2026-09-04 00:09:03 +0100

    feat: adds every prototypes and header files needed by every function declaration

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+unsigned int _pow(unsigned int base, unsigned int power);
+int convert(char c);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif /* ends the if macro */
